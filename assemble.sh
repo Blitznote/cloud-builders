d5d335c22f6582f3ef76346080299f1da7569d3a
Mark Kubacki <wmark@hurrikane.de>
2018-12-31 21:16:26 +0100

    GCE: swap directory /workspace into PWD if it exists
    Google Cloud Build insists on its own directory /workspace, which you
    cannot change even by changing mount-point of volume '/workspace'.
    Which makes symlinking it into the "workdir" a common choire that is
    hereby automated away.

--- assemble.sh
+++ assemble.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 : ${CI_REGISTRY_IMAGE:="blitznote/golang"}
 
 cd "${latest}"
+ln -f ../prepare-workspace.bash
 # First run gets us the most recent version but with tools made by the previous one.
 docker build --rm -t "${CI_REGISTRY_IMAGE}:latest" .
 # Second run builds any tools using it.
@@ -22,6 +23,7 @@ for V in $(ls -1d [1-2].* | sort --version-sort -r); do
   if [[ "${V}" == "${latest}" ]]; then continue; fi
 
   cd "${V}"
+  ln -f ../prepare-workspace.bash
   docker build --rm -t "${CI_REGISTRY_IMAGE}:${V}" .
   docker tag "${CI_REGISTRY_IMAGE}:${V}" \
     "${CI_REGISTRY_IMAGE}:$(docker inspect "${CI_REGISTRY_IMAGE}:${V}" | jq -r '.[0].Config.Labels["org.label-schema.version"]')" \
