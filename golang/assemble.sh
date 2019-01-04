584c702e11d1fbef005ed320685fcc29becd9f19
Mark Kubacki <wmark@hurrikane.de>
2019-01-04 21:43:20 +0100

    golang: use on Dockerfile, just change the variables

--- golang/assemble.sh
+++ golang/assemble.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
 
-set -euo pipefail
-
-: ${latest:="$(ls -1d [1-2].* | sort --version-sort -r | head -n 1)"}
+: ${latest:="$1"}
 : ${CI_REGISTRY_IMAGE:="blitznote/golang"}
 
-cd "${latest}"
-ln -f ../prepare-workspace.bash
-# First run gets us the most recent version but with tools made by the previous one.
-docker build --rm -t "${CI_REGISTRY_IMAGE}:latest" .
-# Second run builds any tools using it.
-docker build --rm -t "${CI_REGISTRY_IMAGE}:latest" .
-cd ..
+set -euo pipefail
 
-docker tag "${CI_REGISTRY_IMAGE}:latest" \
-  "${CI_REGISTRY_IMAGE}:${latest}"
-docker tag "${CI_REGISTRY_IMAGE}:latest" \
-  "${CI_REGISTRY_IMAGE}:$(docker inspect "${CI_REGISTRY_IMAGE}:latest" | jq -r '.[0].Config.Labels["org.label-schema.version"]')" \
-  || true
+# A dummy image which is used to compile any "gotools".
+build_gotools() {
+  pushd .
+  mkdir -p "latest"
+  cd $_
 
-for V in $(ls -1d [1-2].* | sort --version-sort -r); do
-  if [[ "${V}" == "${latest}" ]]; then continue; fi
-
-  cd "${V}"
   ln -f ../prepare-workspace.bash
-  docker build --rm -t "${CI_REGISTRY_IMAGE}:${V}" .
-  docker tag "${CI_REGISTRY_IMAGE}:${V}" \
-    "${CI_REGISTRY_IMAGE}:$(docker inspect "${CI_REGISTRY_IMAGE}:${V}" | jq -r '.[0].Config.Labels["org.label-schema.version"]')" \
-    || true
-  cd ..
+  <../Dockerfile >Dockerfile sed -e "/GOLANG_VERSION=/s@[0-9]*\.[0-9]*[^\"']*@${latest}@"
+  # This deletes the first stage, the one starting with "FROM blitznote/golang:latest as gotools",
+  #   because I don't want to fetch any Golang images from any registry as none might exist or might be untrustworthy.
+  sed -i \
+    -e '/^FROM [^ ]*$/{h;};1,/^FROM/g;/^COPY --from/d' \
+    Dockerfile
+
+  docker build --rm -t "$(<../Dockerfile head -n 1 | cut -d ' ' -f 2)" .
+
+  popd
+}
+
+build_one_version() {
+  local VER="$1"
+  pushd .
+  mkdir -p "${VER}"
+  cd $_
+
+  ln -f ../prepare-workspace.bash
+  <../Dockerfile >Dockerfile sed -e "/GOLANG_VERSION=/s@[0-9]*\.[0-9]*[^\"']*@${VER}@"
+
+  docker build --rm -t "${CI_REGISTRY_IMAGE}:${VER}" .
+  if [[ "${VER}" != *"alpha"* ]] && [[ "${VER}" != *"beta"* ]] && [[ "${VER}" != *"rc"* ]]; then
+    docker tag "${CI_REGISTRY_IMAGE}:${VER}" "${CI_REGISTRY_IMAGE}:${VER%.*}"
+  fi
+  # This will overwrite any preliminary 'latest' image created above.
+  if [[ "${VER}" == "$(printf "${latest}" | cut -d '.' -f -2)"* ]]; then
+    docker tag "${CI_REGISTRY_IMAGE}:${VER}" "${CI_REGISTRY_IMAGE}:latest"
+  fi
+
+  popd
+}
+
+if (( ${#@} <= 0 )); then
+  >&2 printf "Usage: $0 ver=latest ver…\n"
+fi
+
+build_gotools
+for V; do
+  build_one_version "$V"
 done
 
 exit 0
